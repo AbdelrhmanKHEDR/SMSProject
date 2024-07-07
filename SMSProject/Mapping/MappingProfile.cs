983d17f6cc40ab3a328b41b872251ca4a84c7698
abdelrahman.khedr <abdelrahman.khedr@DEV-ITTEST>
2024-07-07 19:58:37 +0300

    Add project files.

---
+++ SMSProject/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SMSProject.Models;
+using SMSProject.ViewModels;
+
+namespace SMSProject.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        { 
+            //Users
+            CreateMap<ApplicationUser , UserViewModel>();
+        
+        
+        }
+    }
+}
---
+++ SMSProject/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SMSProject.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public bool IsDeleted { get; set; } 
+        public DateTime CreaetdOn { get; set; } 
+        public DateTime? LastUpdatedOn { get; set; } 
+    }
+}
